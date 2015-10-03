78a7fba39eaa9042a2c0b528bceb108ad78f7015
Rahul Kale <rahul.kale@outlook.com>
2015-10-03 18:34:37 -0400

    Final Triangular Pattern

--- PrintingPatterns/TrianglePatterns/Program.cs
+++ PrintingPatterns/TrianglePatterns/Program.cs
@@ -4,6 +4,13 @@ namespace TrianglePatterns
 {
 	class Program
 	{
+        static int i;
+        static int count = 10;
+        static int j;
+        static char space = ' ';
+        static char star = '*';
+
+
 		static void Main(string[] args)
 		{
 			DisplayPatternA();
@@ -16,22 +23,73 @@ namespace TrianglePatterns
 
 		static void DisplayPatternA()
 		{
-			// your implementation here
+            Console.WriteLine();
+
+            for (j = 1; j <= count; j++)
+            {
+                for (i = 1; i <= count; i++)
+                {
+                    if(j>= i)
+                    Console.Write(star);
+                }Console.WriteLine();
+            }
+            
 		}
 
 		static void DisplayPatternB()
 		{
-			// your implementation here
-		}
+            // your implementation here
+
+            Console.WriteLine();
+
+            for (j = 1; j <= count; j++)
+            {
+                for (i = 1; i <= count; i++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write(star);
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+        }
 
 		static void DisplayPatternC()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine();
+
+            for (j = 1; j <= count; j++)
+            {
+                for (i = 1; i <= count; i++)
+                {
+                    if (i<=j)
+                        Console.Write(space);
+                    else
+                        Console.Write(star);
+                }
+                Console.WriteLine();
+            }
+        }
 
 		static void DisplayPatternD()
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine();
+
+            for (j = 10; j >= 1; j--)
+            {
+                for (i = 1; i <= count; i++)
+                {
+                    if (i >= j)
+                        Console.Write(star);
+                    else
+                        Console.Write(space);
+                }
+                Console.WriteLine();
+            }
+        }
 	}
 }
