af7c4c268831db146bcefbb036738d2ef21da5b2
Rahul Kale <rahul.kale@outlook.com>
2015-10-02 23:53:41 -0400

    ver 1

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -1,14 +1,47 @@
-﻿namespace FindingPrimeNumbers
+﻿using System;
+
+namespace FindingPrimeNumbers
 {
 	public class FindingPrimeNumbers
 	{
-		public long FindSumOfPrimeNumbers(int n)
+        static bool is_prime(int num)
+        {
+            if (num == 2) return true;
+            int i;
+            for (i = 2; i < num - 1;)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+
+        public long FindSumOfPrimeNumbers(int input)
 		{
 			long sum = 0;
+            int count = 2;
+            int i = 0;
 
-			// your solution goes in here
+            while (i <= input)
+            {
+                if (i == input)
+                {
+                    break;
+                }
 
-			return sum;
+                if (is_prime(count))
+                {
+                    Console.WriteLine("\n" + count);
+                    sum = sum + count;
+                    i++;
+                }
+                count++;
+            }
+
+            return sum;
 		}
 	}
 }
