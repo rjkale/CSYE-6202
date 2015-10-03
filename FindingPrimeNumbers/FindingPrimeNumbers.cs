0d6242a680534ccff35039ef2f2c983999306c27
Rahul Kale <rahul.kale@outlook.com>
2015-10-03 17:03:42 -0400

    Version Oct 3

--- FindingPrimeNumbers/FindingPrimeNumbers.cs
+++ FindingPrimeNumbers/FindingPrimeNumbers.cs
@@ -1,14 +1,47 @@
-﻿namespace FindingPrimeNumbers
+﻿using System;
+
+namespace FindingPrimeNumbers
 {
-	public class FindingPrimeNumbers
-	{
-		public long FindSumOfPrimeNumbers(int n)
-		{
-			long sum = 0;
+    public class FindingPrimeNumbers
+    {
+        static bool is_prime(int num)
+        {
+            if (num == 2) return true;
+            int i;
+            for (i = 2; i < num - 1;)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
 
-			// your solution goes in here
+        public long FindSumOfPrimeNumbers(int input)
+        {
+            long sum = 0;
+            int count = 2;
+            int i = 0;
 
-			return sum;
-		}
-	}
+            while (i <= input)
+            {
+                if (i == input)
+                {
+                    break;
+                }
+
+                if (is_prime(count))
+                {
+                    Console.WriteLine("\n" + count);
+                    sum = sum + count;
+                    i++;
+                }
+                count++;
+            }
+
+            return sum;
+        }
+    }
 }
--- FindingPrimeNumbers/Program.cs
+++ FindingPrimeNumbers/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace FindingPrimeNumbers
+﻿using System;
+
+namespace FindingPrimeNumbers
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+
+            Console.WriteLine("Enter N prime numbers you want");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input == 0)
+            {
+                Console.WriteLine("\n0");
+            }
+            else
+            {
+                FindingPrimeNumbers findprime = new FindingPrimeNumbers();
+                long output = findprime.FindSumOfPrimeNumbers(input);
+
+                Console.WriteLine("\nThe sum of Prime numbers is " + output);
+                Console.ReadLine();
+            }
+
+
+
+
+        }
+    }
 }
-																																																		
