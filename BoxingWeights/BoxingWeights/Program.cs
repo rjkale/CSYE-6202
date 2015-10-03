c07ccc927e92f27046046a36cc6e3b9f23cb3614
Rahul Kale <rahul.kale@outlook.com>
2015-10-03 00:56:48 -0400

    ver 2

--- BoxingWeights/BoxingWeights/Program.cs
+++ BoxingWeights/BoxingWeights/Program.cs
@@ -2,21 +2,20 @@
 
 namespace BoxingWeights
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-           while (true)
-           {
-            Console.WriteLine("Enter your weight to Determince your weight classification");
-            int weight = Convert.ToInt32(Console.ReadLine());
+class Program
+ {
+  static void Main(string[] args)
+  {
+   while (true)
+   {
+    Console.WriteLine("Enter your weight to Determince your weight classification");
+    int weight = Convert.ToInt32(Console.ReadLine());
+    BoxingWeightClassifier bw = new BoxingWeightClassifier();
+    string output = bw.ClassifyBoxingWeight(weight);
 
-            BoxingWeightClassifier bw = new BoxingWeightClassifier();
-            string output = bw.ClassifyBoxingWeight(weight);
-
-            Console.WriteLine(output);
-            Console.ReadLine();
-           }
-        }
-	}
+    Console.WriteLine(output);
+    Console.ReadLine();
+   }
+  }
+ }
 }
