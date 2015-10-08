34bf942e979781d94ea4c70a962a34d58a178a95
Rahul Kale <rahul.kale@outlook.com>
2015-10-07 21:46:43 -0400

    commit 1

--- GasPump/GasPump/Program.cs
+++ GasPump/GasPump/Program.cs
@@ -15,15 +15,43 @@ namespace GasPump
 
 		static void Main(string[] args)
 		{
-			// your implementation here
-		}
+            // your implementation here
+            Console.WriteLine("Welcome! \n Gas Type \n R/r = Regular Gas \n M/m = MidgradeGas \n P/p = PremiumGas \n D/d = DieselFuel ");
+            Console.WriteLine("\nPlease enter the Purchased Gas Type");
+            String input = Convert.ToString(Console.ReadLine());
+            while (true)
+            {
+            
+            if(UserEnteredValidGasType(input))
+            {
+                Console.WriteLine("Its a regular Gas");
+                    char inputchar = Convert.ToChar(input);
+
+                    GasType gt = GasTypeMapper(inputchar);
+                    Double gpm = GasPriceMapper(gt);
+                    Console.WriteLine("You Brought" + inputchar + " and the Price of the gas is " +gpm);
+
+
+                    break;
+                
+            }//end if
+            else
+            {
+                Console.WriteLine("Please enter a valid option");
+                    break;
+            }//end else
+
+            }//End of While loop
+
+            Console.ReadLine();
+        }//End of main function
 
 		// use this method to check and see if sentinel value is entered
 		public static bool UserEnteredSentinelValue(string userInput)
 		{
 			var result = false;
 
-			// your implementation here
+			
 
 			return result;
 		}
@@ -34,7 +62,10 @@ namespace GasPump
 		{
 			var result = false;
 
-			// your implementation here
+            if(userInput == "r" || userInput == "R")
+            {
+                result = true;
+            }
 			
 			return result;
 		}
@@ -45,7 +76,7 @@ namespace GasPump
 		{
 			var result = false;
 
-			// your implementation here
+			
 
 			return result;
 		}
@@ -55,25 +86,54 @@ namespace GasPump
 		// this mapping "must" be used within CalculateTotalCost() method later on
 		public static GasType GasTypeMapper(char c)
 		{
-			GasType gasType = GasType.None;
+			
 
-			// your implementation here
+            switch (c)
+            {
 
-			return gasType;
-		}
+                case 'R':
+                case 'r':
+                    {
+                        GasType gasType = GasType.RegularGas;
+                        return gasType;
+                    }
+
+                default: throw new ArgumentOutOfRangeException();
+            }//end of switch
+
+            
+            //GasType gasType = GasType.None;
+        }
 
 		public static double GasPriceMapper(GasType gasType)
-		{
+        {
 			var result = 0.0;
+            switch (gasType)
+            {
+                case GasType.None:
+                    return 0;
+                    
+                case GasType.RegularGas:
+                    return 1.94;
+                    
+                case GasType.MidgradeGas:
+                    return 2.00;
+                    
+                case GasType.PremiumGas:
+                    return 2.24;
 
-			// your implementation here
+                case GasType.DieselFuel:
+                    return 2.17;
 
-			return result;
-	}
+            }
+
+
+            return result;
+	    }
 
 		public static void CalculateTotalCost(GasType gasType, int gasAmount, ref double totalCost)
 		{
-			// your implementation here
+			
 		}
 	}
 }
