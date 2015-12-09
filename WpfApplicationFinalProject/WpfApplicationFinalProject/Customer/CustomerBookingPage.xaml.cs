f95430210b6483f1cd404f51fd877766ec942502
Rahul Kale <rahul.kale@outlook.com>
2015-12-09 01:40:02 -0500

    commit 9th dec

---
+++ WpfApplicationFinalProject/WpfApplicationFinalProject/Customer/CustomerBookingPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApplicationFinalProject.Class;
+
+namespace WpfApplicationFinalProject.Customer
+{
+    /// <summary>
+    /// Interaction logic for CustomerBookingPage.xaml
+    /// </summary>
+    public partial class CustomerBookingPage : Window
+    {
+        Flight flight;
+        public CustomerBookingPage(Flight flight)
+        {
+            InitializeComponent();
+            this.flight = flight;
+            MessageBox.Show(flight.flightName);
+        }
+    }
+}
--- WpfApplicationFinalProject/WpfApplicationFinalProject/Customer/CustomerHomePageWindow.xaml.cs
+++ WpfApplicationFinalProject/WpfApplicationFinalProject/Customer/CustomerHomePageWindow.xaml.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-
+using WpfApplicationFinalProject.Class;
+using WpfApplicationFinalProject.Customer;
+using WpfApplicationFinalProject.HomePage_and_Signup;
 
 namespace WpfApplicationFinalProject
 {
@@ -13,19 +15,94 @@ namespace WpfApplicationFinalProject
     /// </summary>
     public partial class CustomerHomePageWindow : Window
     {
+        
         public CustomerHomePageWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            populateCombobox();
+            DatePicker.SelectedDate = new DateTime(2015, 12, 11);
 
-
-         
+            
         }
 
-        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
             MainWindow main = new MainWindow();
             main.Show();
+        }
+
+        private void populateCombobox()
+        {
+            string[] str = new string[] { "NY", "Boston", "Chicago", "Rhode Island", "Houston", "Austin", "Seattle", "San Fransisco", "Washinton DC", "Los Angeles", "Philadelphia", "Portland", "Atlanta" };
+
+            foreach (var item in str)
+            {
+                coBoxSourceCity.Items.Add(item);
+                coBoxDestinationCity.Items.Add(item);
+
+            }
+            coBoxSourceCity.SelectedIndex = 1;
+            coBoxDestinationCity.SelectedIndex = 2;
+
+
+            string[] classs = new string[] { "Economy", "Economy Plus", "Business" };
+
+            foreach (var item in classs)
+            {
+                coboxClass.Items.Add(item);
+            }
+            coboxClass.SelectedIndex = 0;
+
+            for (int i = 1; i < 10; i++)
+            {
+                coboxSeats.Items.Add(i);
+            }
+            coboxSeats.SelectedIndex = 0;
+
+        }
+
+
+
+        private Boolean checkforvalues()
+        {
+            string source = coBoxSourceCity.SelectedValue.ToString();
+            string destination = coBoxDestinationCity.SelectedValue.ToString();
+            string date = DatePicker.ToString();
+            if (source == destination)
+            {
+                return false;
+            }
+            else if (date == "")
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkforvalues() == true)
+            {
+                Search search = new Search();
+                search.sourceCity = coBoxSourceCity.SelectedValue.ToString();
+                search.destinationCity = coBoxDestinationCity.SelectedValue.ToString();
+                search.classType = coboxClass.SelectedValue.ToString();
+                search.date = DatePicker.SelectedDate.Value.ToShortDateString();
+                search.seats = coboxSeats.SelectedValue.ToString();
+
+
+                CustomerSearchPageWindow c = new CustomerSearchPageWindow(search);
+                c.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Please select different Source and Destination cities \n OR \nSelect a date");
+            }
         }
     }
 }
