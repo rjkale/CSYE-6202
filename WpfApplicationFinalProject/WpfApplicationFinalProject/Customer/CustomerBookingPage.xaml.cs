c7919eae04588c6a177e73c8d27e873506b1de48
Rahul Kale <rahul.kale@outlook.com>
2015-12-09 02:59:34 -0500

    commit of customer booking

--- WpfApplicationFinalProject/WpfApplicationFinalProject/Customer/CustomerBookingPage.xaml.cs
+++ WpfApplicationFinalProject/WpfApplicationFinalProject/Customer/CustomerBookingPage.xaml.cs
@@ -25,7 +25,6 @@ namespace WpfApplicationFinalProject.Customer
         {
             InitializeComponent();
             this.flight = flight;
-            MessageBox.Show(flight.flightName);
         }
     }
 }
